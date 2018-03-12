c9f104164e7e1a7c869cf12f02df936df338e99b
Chris Henry <cdhenry@gmail.com>
2018-03-12 21:51:50 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class application
+
+  def  call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path == "//"
+
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+  end
+  
+end
